90e76ddf14be79b81cafdad0927960472b838654
Tom Christensen <pavera@gmail.com>
2018-06-24 07:56:06 -0600

    Converting to using decimal for maths as it provides more flexibility in rounding.

--- gradeomatic.py
+++ gradeomatic.py
@@ -6,14 +6,14 @@ valid_scales = ['Fahrenheit', 'Celsius', 'Kelvin', 'Rankine']
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('orig_temp', type=float, nargs=1,
+    parser.add_argument('orig_temp', type=str, nargs=1,
                         help='Temperature to be converted')
     parser.add_argument('orig_scale', type=str, nargs=1, choices=valid_scales)
     parser.add_argument('dest_scale', type=str, nargs=1, choices=valid_scales)
-    parser.add_argument('student_answer', type=float, nargs=1)
+    parser.add_argument('student_answer', type=str, nargs=1)
 
     args = parser.parse_args()
     tc = TempConverter()
-    tc.run_conversion(args.orig_temp[0], args.orig_scale[0], args.dest_scale[0], args.student_answer[0])
+    print(tc.run_conversion(args.orig_temp[0], args.orig_scale[0], args.dest_scale[0], args.student_answer[0]))
 
 
--- temp_converters/__init__.py
+++ temp_converters/__init__.py
@@ -1,64 +1,82 @@
+from decimal import Decimal
+
+
 class InvalidTemp(Exception):
     pass
 
 
 class TempConverter(object):
 
-    def f_to_c(self, temp_f):
-        return (temp_f - 32) * 5/9.0
+    @staticmethod
+    def f_to_c(temp_f):
+        return (temp_f - 32) * 5/Decimal('9.0')
 
-    def f_to_k(self, temp_f):
-        return (temp_f + 459.67) * 5/9.0
+    @staticmethod
+    def f_to_k(temp_f):
+        return (temp_f + Decimal('459.67')) * 5/Decimal('9.0')
 
-    def f_to_r(self, temp_f):
-        return temp_f + 459.67
+    @staticmethod
+    def f_to_r(temp_f):
+        return temp_f + Decimal('459.67')
 
-    def c_to_f(self, temp_c):
-        return (temp_c * 9/5.0) + 32
+    @staticmethod
+    def c_to_f(temp_c):
+        return (temp_c * 9/Decimal('5.0')) + 32
 
-    def c_to_k(self, temp_c):
-        return temp_c + 273.15
+    @staticmethod
+    def c_to_k(temp_c):
+        return temp_c + Decimal('273.15')
 
-    def c_to_r(self, temp_c):
-        return (temp_c * 9/5.0) + 491.67
+    @staticmethod
+    def c_to_r(temp_c):
+        return (temp_c * 9/Decimal('5.0')) + Decimal('491.67')
 
-    def k_to_c(self, temp_k):
+    @staticmethod
+    def k_to_c(temp_k):
         if temp_k < 0:
             raise InvalidTemp("Invalid Temp")
-        return temp_k - 273.15
+        return temp_k - Decimal('273.15')
 
-    def k_to_f(self, temp_k):
+    @staticmethod
+    def k_to_f(temp_k):
         if temp_k < 0:
             raise InvalidTemp("Invalid Temp")
-        return (temp_k * 9/5.0) - 459.67
+        return (temp_k * 9/Decimal('5.0')) - Decimal('459.67')
 
-    def k_to_r(self, temp_k):
+    @staticmethod
+    def k_to_r(temp_k):
         if temp_k < 0:
             raise InvalidTemp("Invalid Temp")
-        return temp_k * 9/5.0
+        return temp_k * 9/Decimal('5.0')
 
-    def r_to_c(self, temp_r):
+    @staticmethod
+    def r_to_c(temp_r):
         if temp_r < 0:
             raise InvalidTemp("Invalid Temp")
-        return (temp_r - 491.67) * 5/9.0
+        return (temp_r - Decimal('491.67')) * 5/Decimal('9.0')
 
-    def r_to_f(self, temp_r):
+    @staticmethod
+    def r_to_f(temp_r):
         if temp_r < 0:
             raise InvalidTemp("Invalid Temp")
-        return temp_r - 459.67
+        return temp_r - Decimal('459.67')
 
-    def r_to_k(self, temp_r):
+    @staticmethod
+    def r_to_k(temp_r):
         if temp_r < 0:
             raise InvalidTemp("Invalid Temp")
-        return temp_r * 5/9.0
+        return temp_r * 5/Decimal('9.0')
 
     def run_conversion(self, orig_temp, orig_scale, dest_scale, student_answer):
         conversion = "{0}_to_{1}".format(orig_scale[0].lower(), dest_scale[0].lower())
         try:
-            answer = getattr(self, conversion)(orig_temp)
-            if round(answer) == round(student_answer):
-                print("correct")
+            answer = getattr(self, conversion)(Decimal(orig_temp))
+            if round(answer) == round(Decimal(student_answer)):
+                result = "correct"
             else:
-                print("incorrect")
+                print("answer: {0}, student_answer: {1}".format(round(answer), round(Decimal(student_answer))))
+                result = "incorrect"
         except InvalidTemp as exc:
-            print("invalid")
+            result = "invalid"
+
+        return result
